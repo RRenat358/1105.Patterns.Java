dcc3faea429f7cdeed78caa19012eebafc505448
rrenat358 <rrenat358@gmail.com>
2022-06-27 17:37:03 +0300

    1736. WaterSpring getInstance()

---
+++ 01_Singleton/src/ru/rrenat358/singleton/WaterSpring.java
@@ -0,0 +1,20 @@
+package ru.rrenat358.singleton;
+
+public class WaterSpring {
+
+    private static WaterSpring instance = null;
+
+    private static String kindOfWater = "Литров чистой воды";
+
+    public static WaterSpring getInstance() {
+        if (instance == null) {
+            instance = new WaterSpring();
+        }
+        return instance;
+    }
+
+    public String getWater(float liter) {
+        return (liter + kindOfWater);
+    }
+
+}
