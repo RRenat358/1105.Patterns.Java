5733099b79a6200e6e5aa9d2a39ae2c4ba1156af
rrenat358 <rrenat358@gmail.com>
2022-06-29 13:10:51 +0300

    1310

--- 02_Factory/src/FactoryApp.java
+++ 02_Factory/src/FactoryApp.java
@@ -1,5 +1,19 @@
 public class FactoryApp {
     public static void main(String[] args) {
+        System.out.println("\n==============================");
+        System.out.println("=== variants.Singleton ================\n");
+
+
+
+
+
+
+
+
+
+
+        System.out.println("\n==============================");
+        System.out.println("==============================\n");
 
     }
 
---
+++ 02_Factory/src/Store.java
@@ -0,0 +1,5 @@
+public interface Store {
+
+    void saleGood();
+
+}
