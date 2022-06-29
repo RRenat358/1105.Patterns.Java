081eb192dce800b80e492d28258e9c5e6374bab6
rrenat358 <rrenat358@gmail.com>
2022-06-29 17:28:21 +0300

    1727. Create Builder

---
+++ 03_Builder/src/BuilderApp.java
@@ -0,0 +1,25 @@
+
+
+public class BuilderApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n==============================");
+        System.out.println("=== Builder ==================\n");
+
+
+
+
+
+
+
+        System.out.println("\n==============================");
+        System.out.println("==============================\n");
+
+
+
+
+    }
+
+
+}
