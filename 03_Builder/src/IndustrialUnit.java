fdf225c1b15b0d2c5dbfc5268512da0b5bb19778
rrenat358 <rrenat358@gmail.com>
2022-07-20 22:55:27 +0300

    2255

--- 03_Builder/src/IndustrialUnit.java
+++ 03_Builder/src/IndustrialUnit.java
@@ -1,5 +1,54 @@
+
+/**
+ * Промышленный объект
+ *
+ */
+
+
 public class IndustrialUnit {
 
+    private String Name;
+    private  float Temperature;
+    private double Pressure;
+    private int Voltage;
+    private String SupplierName;
+    private OperationStatus operationStatus;
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setTemperature(float temperature) {
+        Temperature = temperature;
+    }
+
+    public void setPressure(double pressure) {
+        Pressure = pressure;
+    }
+
+    public void setVoltage(int voltage) {
+        Voltage = voltage;
+    }
+
+    public void setSupplierName(String supplierName) {
+        SupplierName = supplierName;
+    }
+
+    public void setOperationStatus(OperationStatus operationStatus) {
+        this.operationStatus = operationStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "IndustrialUnit{" +
+                "Name='" + Name + '\'' +
+                ", Temperature=" + Temperature +
+                ", Pressure=" + Pressure +
+                ", Voltage=" + Voltage +
+                ", SupplierName='" + SupplierName + '\'' +
+                ", operationStatus=" + operationStatus +
+                '}';
+    }
 
 
 }
