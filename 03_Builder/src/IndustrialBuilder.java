0f298761efaa43d525e6c1128f4bccddea50000f
rrenat358 <rrenat358@gmail.com>
2022-07-21 16:47:50 +0300

    1647

--- 03_Builder/src/IndustrialBuilder.java
+++ 03_Builder/src/IndustrialBuilder.java
@@ -3,11 +3,25 @@
  */
 
 
-public class IndustrialBuilder {
+public abstract class IndustrialBuilder {
 
     IndustrialUnit industrialUnit;
 
 
+    void createIndustrial() {
+        industrialUnit = new IndustrialUnit();
+    }
+
+    abstract void buildName();
+    abstract void buildTemperature();
+    abstract void buildPressure();
+    abstract void buildVoltage();
+    abstract void buildSupplierName();
+    abstract void buildOperation();
+
+    IndustrialUnit getIndustrialUnit() {
+        return industrialUnit;
+    }
 
 
 }
