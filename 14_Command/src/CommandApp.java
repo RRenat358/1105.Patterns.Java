a9480bc5261644f3f363fd3c5e1fe2c031984211
rrenat358 <rrenat358@gmail.com>
2022-07-28 22:45:07 +0300

    2245

---
+++ 14_Command/src/CommandApp.java
@@ -0,0 +1,29 @@
+
+
+public class CommandApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n==============================");
+        System.out.println("=== Command ==================\n");
+
+
+
+
+
+
+
+
+
+
+        System.out.println("\n=== END ======================");
+        System.out.println("==============================\n");
+
+
+
+
+    }
+
+
+}
+
