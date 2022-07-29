2a5a31dd42222426012c659ed64e91534c739458
rrenat358 <rrenat358@gmail.com>
2022-07-29 21:33:21 +0300

    2133

---
+++ 14_Command/src/MusicBox.java
@@ -0,0 +1,23 @@
+/**
+ * Музыкальный центр
+ *
+ */
+
+
+public class MusicBox {
+
+    public void turnOn() {
+        System.out.println("MusicBox is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("MusicBox is OFF");
+    }
+
+    public void makeLoud() {
+        System.out.println("Sound is Loud");
+    }
+
+
+
+}
