6f7e694c86e06b8d50a9d8d35139fd73d2d23ed8
rrenat358 <rrenat358@gmail.com>
2022-07-30 13:37:07 +0300

    1337

--- 15_Iterator/src/MarketStore.java
+++ 15_Iterator/src/MarketStore.java
@@ -3,7 +3,7 @@
  */
 
 
-public class MarketStore {
+public class MarketStore implements Collection{
 
     private String name;
     private String[] goods;
@@ -17,8 +17,86 @@ public class MarketStore {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String[] getGoods() {
         return goods;
+    }
+
+    public void setGoods(String[] goods) {
+        this.goods = goods;
+    }
+
+    @Override
+    public MyIterator getIterator() {
+        return new MarketStoreIterator();
+    }
+
+
+    //Конкретный Итератор склада
+    public class MarketStoreIterator implements MyIterator {
+
+        int index;
+        MarketStore marketStore;
+
+
+        @Override
+        public boolean hasNext() {
+            if (index < goods.length) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            return goods[index++];
+        }
+
+
+        @Override
+        public boolean hasPrevious() {
+            if (index > 0) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object previous() {
+            index--;
+            return goods[index];
+        }
+
+
+        @Override
+        public int getIteratorPosition() {
+            return index;
+        }
+
+        @Override
+        public Object getItem(int id) {
+            return goods[id];
+        }
+
+        @Override
+        public int setIteratorPosition(int position) {
+            index = position;
+            return index;
+        }
+
+        @Override
+        public boolean removItem(int id) {
+            if (id < goods.length) {
+                goods[id] = "[DELETED]";
+                return true;
+            }
+            return false;
+        }
+
+
     }
 
 
---
+++ 15_Iterator/src/MarketStoreIterator.java
@@ -0,0 +1,55 @@
+
+
+
+/*
+
+public class MarketStoreIterator implements MyIterator {
+
+    int index;
+    MarketStore marketStore;
+
+
+    @Override
+    public boolean hasNext() {
+        if (index < marketStore.getGoods().length) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return marketStore.setGoods(index++);
+    }
+
+    @Override
+    public boolean hasPrevious() {
+        return false;
+    }
+
+    @Override
+    public Object previous() {
+        return null;
+    }
+
+    @Override
+    public int getIteratorPosition() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int id) {
+        return null;
+    }
+
+    @Override
+    public int setIteratorPosition(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean removItem(int id) {
+        return false;
+    }
+}
+*/
