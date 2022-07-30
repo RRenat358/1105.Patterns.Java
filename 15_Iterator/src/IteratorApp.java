e5407c6a46707f4c3feddd7389a499cab0206944
rrenat358 <rrenat358@gmail.com>
2022-07-30 17:56:17 +0300

    1756

--- 15_Iterator/src/IteratorApp.java
+++ 15_Iterator/src/IteratorApp.java
@@ -20,18 +20,64 @@ public class IteratorApp {
 
         MyIterator myIterator = marketStore.getIterator();
 
+        int itemPosition = 1;
 
-        System.out.println("\n=== 1 ========================");
+
+        System.out.println("\n\n=== 1 ======================================================");
 
         System.out.println(marketStore.getName());
 
-        System.out.println("\n=== 2 ========================");
+
+        System.out.println("\n\n=== 2 ======================================================");
+
 
         while (myIterator.hasNext()) {
-            System.out.println(1 + ". " + myIterator.next().toString());
+            System.out.println(itemPosition++ + ". " + myIterator.next().toString());
         }
 
-        System.out.println("\n=== END ======================");
+        System.out.println("\n\n=== 3 ======================================================");
+
+        int itemId = 9;
+        if (itemId > 0 && itemId - 1 < goods.length) {
+            System.out.println(itemId + ". " + myIterator.getItem(itemId - 1));
+        } else {
+            System.out.println("That good # " + itemId + " is absent.");
+        }
+
+
+        System.out.println("\n\n=== 4 ======================================================");
+
+        itemPosition = goods.length;
+        while (myIterator.hasPrevious()) {
+            System.out.println(itemPosition-- + ". " + myIterator.previous().toString());
+        }
+
+        System.out.println("\n\n=== 5 ======================================================");
+
+        String searchNameItem = "Wine";
+        itemPosition = 0;
+        while (myIterator.hasNext()) {
+            String itemTemp = myIterator.next().toString();
+            if (itemTemp.equals(searchNameItem)) {
+                itemPosition = myIterator.getIteratorPosition();
+                System.out.println("Id of [" + searchNameItem + "] == " + itemPosition);
+            }
+        }
+        if (itemPosition == 0) {
+            System.out.println("Id of [" + searchNameItem + "] ==" + " is absent");
+        }
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println("\n\n=== END ======================");
         System.out.println("==============================\n");
 
 
