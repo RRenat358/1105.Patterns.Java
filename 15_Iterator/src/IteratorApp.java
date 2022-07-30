96f62b45d78ef9c6049cdb832bb8e9370a1b0223
rrenat358 <rrenat358@gmail.com>
2022-07-30 17:13:15 +0300

    1713

--- 15_Iterator/src/IteratorApp.java
+++ 15_Iterator/src/IteratorApp.java
@@ -11,24 +11,28 @@ public class IteratorApp {
 
         String[] goods = {
                 "Apple", "Orange", "Banana",
-                "Water", "Juice",
-                "Fish", "Blanca",
-                "Disk", "Monitor", "Refrigerator", "TV",
-                "Book",
-                "Soap"
+                "Water", "Juice", "Wine",
+                "Fish", "Blanca", "Beef",
+                "Book", "Disk", "Lamp",
         };
 
+        MarketStore marketStore = new MarketStore("ChaosStore", goods);
+
+        MyIterator myIterator = marketStore.getIterator();
 
 
+        System.out.println("\n=== 1 ========================");
 
+        System.out.println(marketStore.getName());
 
+        System.out.println("\n=== 2 ========================");
 
-
+        while (myIterator.hasNext()) {
+            System.out.println(1 + ". " + myIterator.next().toString());
+        }
 
         System.out.println("\n=== END ======================");
         System.out.println("==============================\n");
-
-
 
 
     }
