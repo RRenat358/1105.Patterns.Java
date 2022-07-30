bbc2488b437bf900e7f0be1d7c5584a9ad68be06
rrenat358 <rrenat358@gmail.com>
2022-07-30 11:05:15 +0300

    1105

---
+++ 15_Iterator/src/IteratorApp.java
@@ -0,0 +1,34 @@
+/**
+ * Iterator
+ */
+
+
+public class IteratorApp {
+    public static void main(String[] args) {
+
+        System.out.println("\n==============================");
+        System.out.println("=== Iterator ==================\n");
+
+        String[] goods = {
+                "Apple", "Orange", "Banana",
+                "Book",
+                "Disk", "Monitor", "Refrigerator", "TV",
+                "Fish", "Blanca",
+                "Water", "Juice",
+                "Soap"
+        };
+
+
+
+
+
+
+        System.out.println("\n=== END ======================");
+        System.out.println("==============================\n");
+
+
+
+
+    }
+
+}
